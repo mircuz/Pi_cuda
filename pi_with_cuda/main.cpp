46220757ad057a118d83dfe8f1f7f95a74c9519f
Mirco <mirco.meaz@gmail.com>
2019-10-21 13:09:48 +0200

    Initial Commit

---
+++ pi_with_cuda/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pi_with_cuda
+//
+//  Created by Mirco Meazzo on 21/10/2019.
+//  Copyright © 2019 Mirco Meazzo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
