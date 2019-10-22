c5c796accf2f8ecd1ca8339c4ce51d0c6dd1cd89
Mirco <mirco.meaz@gmail.com>
2019-10-22 12:07:35 +0200

    Pi without CUDA

--- pi_with_cuda/main.cpp
+++ pi_with_cuda/main.cpp
@@ -7,9 +7,42 @@
 //
 
 #include <iostream>
+#include <stdlib.h>
+#include <math.h>
+
+
+#define NLIM 100000000
+
+void compute_r(int &mem) {
+    int inner=0, iter=0;
+        double rand_real, rand_imag, r;
+    
+        for (iter=0; iter<int(NLIM); iter++) {
+        
+            rand_real = double(rand()) / double(RAND_MAX);
+            rand_imag = double(rand()) / double(RAND_MAX);
+            
+            r = sqrt(rand_real*rand_real + rand_imag*rand_imag);
+            
+            if (r <= 1.0f) {
+                inner++;
+            }
+            
+        }
+
+        mem = inner;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    std::cout << "Refine Pi using " << NLIM << " iterations" << std::endl;
+    
+    double pi;
+    int inner;
+    compute_r(inner);
+    
+    pi= 4.0f* (inner/double(NLIM));
+    std::cout << "Pi is " << pi << std::endl;
+    
     return 0;
 }
