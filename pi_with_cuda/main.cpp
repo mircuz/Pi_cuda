b52465691ca8af78b48aa6fe84188375fb0d1648
Mirco <mirco.meaz@gmail.com>
2019-10-23 22:55:36 +0200

    Single GPU thread works

--- pi_with_cuda/main.cpp
+++ pi_with_cuda/main.cpp
@@ -9,25 +9,21 @@
 #include <iostream>
 #include <stdlib.h>
 #include <math.h>
-#include <chrono>
+#include <typeinfo>
 
 
+#define NLIM 10000000
 
 
-#define NLIM 100000000
-
-void compute_r(int *mem, double* rand_real, double* rand_imag ) {
-    int iter=0;
-    double r;
-   
-    for (iter=0; iter<int(NLIM); iter++) {
+__global__ void compute_r(int *mem, double *rand_real, double *rand_imag ) {
+    
+    for (int i=0; i<(int(NLIM)-1); i++) {
         
-        r = sqrt(rand_real[iter]*rand_real[iter] + rand_imag[iter]*rand_imag[iter]);
-        if (r <= 1.0f) {
-            mem[iter] = 1;
+        if ((sqrt(rand_real[i]*rand_real[i] + rand_imag[i]*rand_imag[i])) <= 1.0f) {
+            mem[i] = 1;
         }
         else
-            mem[iter] = 0;
+            mem[i] = 0;
     }
 }
 
@@ -37,31 +33,35 @@ int main(int argc, const char * argv[]) {
     
     double pi;
     int inner=0;
-    int* gpu_inner;
-    double *rand_imag; double *rand_real;
-    gpu_inner = new int[NLIM];
-    rand_real = new double[NLIM];
-    rand_imag = new double[NLIM];
-//    cudaMallocManaged(&gpu_inner,sizeof(int)*int(NLIM));
-//    cudaMallocManaged(&rand_real,sizeof(double)*int(NLIM));
-//    cudaMallocManaged(&rand_imag,sizeof(double)*int(NLIM));
-   
-    for (int i=0; i<int(NLIM); i++) {
+    int *gpu_inner;
+    double *rand_imag, *rand_real;
+//    gpu_inner = new int[NLIM];
+//    rand_real = new double[NLIM];
+//    rand_imag = new double[NLIM];
+    cudaMallocManaged(&gpu_inner,int(NLIM)*sizeof(int));
+    cudaMallocManaged(&rand_real,int(NLIM)*sizeof(double));
+    cudaMallocManaged(&rand_imag,int(NLIM)*sizeof(double));
+    
+ 
+    for (int i=0; i<(int(NLIM)-1); i++) {
         rand_real[i] = double(rand()) / double(RAND_MAX);
         rand_imag[i] = double(rand()) / double(RAND_MAX);
     }
+
+    compute_r <<<1, 1>>> (gpu_inner, rand_real, rand_imag);
+// compute_r (gpu_inner,rand_real,rand_imag);
     
-//    compute_r<<1,1>> (gpu_inner,rand_real,rand_imag);
- compute_r (gpu_inner,rand_real,rand_imag);
-    
-//    cudaDeviceSynchronize();
-    
+    cudaDeviceSynchronize();
+
     for (int i=0; i<int(NLIM); i++) {
         inner += gpu_inner[i];
     }
-    
+
     pi= 4.0f* (inner/double(NLIM));
     std::cout << "Pi is " << pi << std::endl;
-    
+    cudaFree(gpu_inner);
+    cudaFree(rand_imag);
+    cudaFree(rand_real);
+
     return 0;
 }
