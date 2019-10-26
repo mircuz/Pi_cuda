ef0e65bd809496661ae52215a23293099657ce91
Mirco <mirco.meaz@gmail.com>
2019-10-26 13:55:29 +0200

    C++ version without CUDA

--- pi_with_cuda/main.cpp
+++ pi_with_cuda/main.cpp
@@ -15,14 +15,9 @@
 #define NLIM 10000000
 
 
-__global__ void compute_r(int *mem, double *rand_real, double *rand_imag ) {
+ void compute_r(int *mem, double *rand_real, double *rand_imag ) {
     
-    // We execute (total_blocks * blockDim * n_threads) parallel threads per iteration
-    // Execute (total_blocks * blockDim) parallel processes per iteration
-    int index = threadIdx.x + blockIdx.x * blockDim.x;
-    int total_blocks= gridDim.x;
-    // Stride must be total_blocks * blockDim
-    int stride= blockDim.x * total_blocks;
+   int index=0, stride=1;
 
     for (int i=index; i<(int(NLIM)-1); i+=stride) {
         
@@ -42,12 +37,10 @@ int main(int argc, const char * argv[]) {
     int inner=0;
     int *gpu_inner;
     double *rand_imag, *rand_real;
-//    gpu_inner = new int[NLIM];
-//    rand_real = new double[NLIM];
-//    rand_imag = new double[NLIM];
-    cudaMallocManaged(&gpu_inner,int(NLIM)*sizeof(int));
-    cudaMallocManaged(&rand_real,int(NLIM)*sizeof(double));
-    cudaMallocManaged(&rand_imag,int(NLIM)*sizeof(double));
+    gpu_inner = new int[NLIM];
+    rand_real = new double[NLIM];
+    rand_imag = new double[NLIM];
+   
     
  
     for (int i=0; i<(int(NLIM )-1); i++) {
@@ -57,10 +50,10 @@ int main(int argc, const char * argv[]) {
 
     int block_size = 256;
     int n_blocks = (int(NLIM) + block_size - 1) / block_size;   //Ensure (n_blocks * block_size) > NLIM
-    compute_r <<<n_blocks, block_size>>> (gpu_inner, rand_real, rand_imag);
-// compute_r (gpu_inner,rand_real,rand_imag);
+//    compute_r <<<n_blocks, block_size>>> (gpu_inner, rand_real, rand_imag);
+    compute_r (gpu_inner,rand_real,rand_imag);
     
-    cudaDeviceSynchronize();
+ //   cudaDeviceSynchronize();
 
     for (int i=0; i<int(NLIM); i++) {
         inner += gpu_inner[i];
@@ -68,9 +61,7 @@ int main(int argc, const char * argv[]) {
 
     pi= 4.0f* (inner/double(NLIM));
     std::cout << "Pi is " << pi << std::endl;
-    cudaFree(gpu_inner);
-    cudaFree(rand_imag);
-    cudaFree(rand_real);
+
 
     return 0;
 }
